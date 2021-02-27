aae16ee9cd420ae00f5ea584817cdad1bd665922
jean113 <jeanluizdias@gmail.com>
2021-02-27 09:17:05 -0300

    Telas de cadastrar livro autores editora

---
+++ resources/views/editoras/editoras-criar.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../res/styles/edicao.css">
+    <link rel="stylesheet" href="../res/styles/font-awesome/css/font-awesome.min.css">
+    <title>Cadastro de Editoras</title>
+</head>
+<body>
+
+    <div class="conteudo">
+
+        <header>
+
+            
+            <h1>Cadastro de Editoras</h1>
+
+            <a href="/editoras"><i class="fa fa-arrow-left"></i>Voltar</a>
+
+
+        </header>
+
+        <main>
+            <form role="form" action="/editoras/criar" method="post">
+                <div class="campos">
+                    <div>
+                        <label for="">Nome</label>
+                        <input type="text" name="nome"  maxlength="50" required/>
+                    </div>
+
+                    <div>
+                        <label for="">Telefone</label>
+                        <input type="tel" name="telefone" maxlength="17"
+                        pattern="(\([0-9]{3}\))?(([0-9]){2})?([0-9]){5}-([0-9]){4}"/>
+                    </div>
+
+                    <div>
+                        <label for="">E-mail</label>
+                        <input type="email" name="email"  maxlength="30"/>
+                    </div>
+
+                    <div>
+                        <label for="">Observação</label>
+                        <textarea name="obs" maxlength="100" > </textarea>
+        
+                    </div>
+                </div>
+                
+                <button type="submit" class="botao_cadastrar">Salvar</button>
+            </form>
+    </main>
+
+    </div>
+
+
+    
+
+    
+</body>
+</html>
---
+++ resources/views/livros/livros-criar.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../res/styles/edicao.css">
+    <link rel="stylesheet" href="../res/styles/font-awesome/css/font-awesome.min.css">
+    <title>Cadastro de Livros</title>
+</head>
+<body>
+
+    <div class="conteudo">
+
+        <header>
+
+            <h1>Cadastro de Livros</h1>
+
+            <a href="/livros"><i class="fa fa-arrow-left"></i>Voltar</a>
+
+        </header>
+
+        <main>
+
+            <form role="form" action="/livros/criar" method="post">
+
+                <div class="campos">
+    
+                    <div>
+                        <label for="">Título</label>
+                        <input type="text" name="titulo"  maxlength="25" required/>
+                    </div>
+                    
+                    <div>
+                        <label for="">Autor</label>
+                    <select id="autor" name="autor" required>
+                        <option value="" disabled selected>Selecione uma opção</option>
+                        {loop="$DATA"}
+                            <option value={$value.id}>{$value.nome}</option>
+                        {/loop}
+                    </select>
+                    </div>
+                    
+                    <div>
+                        <label for="">Editora</label>
+                        <select id="editora" name="editora" required>
+                            <option value="" disabled selected>Selecione uma opção</option>
+                            {loop="$DATA2"}
+                                <option value={$value.id}>{$value.nome}</option>
+                            {/loop}
+                        </select>
+                    </div>
+        
+                    <div>
+                        <label for="">Data da edição</label>
+                        <input type="date" name="dtEdicao" required/>
+                    </div>
+        
+                    <div>
+                        <label for="">Páginas</label>
+                    <input type="number" name="paginas"/>
+        
+                    </div>
+                    
+                    <div>
+                        <label for="">Tipo de Impressão</label>
+                    <input type="text" name="impressao" maxlength="15"/>
+        
+                    </div>
+                    
+        
+                    <div>
+                        <label for="">Descrição</label>
+                        <textarea name="descricao" maxlength="200"></textarea>
+        
+                    </div>
+    
+                </div>
+    
+                <button type="submit" class="botao_cadastrar">Salvar</button>
+            </form>
+
+        </main>
+
+
+        
+    </div>
+
+    
+</body>
+</html>
