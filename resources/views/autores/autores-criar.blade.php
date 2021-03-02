374c0289e88631f038b4d20ff6f6c52ca8456d3c
jean113 <jeanluizdias@gmail.com>
2021-03-02 07:57:23 -0300

    Template autores criar

--- resources/views/autores/autores-criar.blade.php
+++ resources/views/autores/autores-criar.blade.php
@@ -1,64 +1,47 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../res/styles/edicao.css">
-    <link rel="stylesheet" href="../res/styles/font-awesome/css/font-awesome.min.css">
-    
-    <title>Cadastro de Autores</title>
-</head>
-<body>
+@extends('template.criar')
+@section('conteudo')
 
-    <div class="conteudo">
+    <header>
 
-        <header>
+        <h1>Cadastro de Autores</h1>
 
-            <h1>Cadastro de Autores</h1>
+        <a href=""><i class="fa fa-arrow-left"></i>Voltar</a>
 
-            <a href="/autores"><i class="fa fa-arrow-left"></i>Voltar</a>
+    </header>
 
-        </header>
+    <main>
 
-        <main>
-
-            <form role="form" action="/autores/criar" method="post">
-                <div class="campos">
-                    <div>
-                        <label for="">Nome</label>
-                        <input type="text" name="nome" maxlength="50" required/>
-                    </div>
-    
-                    <div>
-                        <label for="">Telefone</label>
-                        <input type="tel" name="telefone" maxlength="17"
-                        pattern="(\([0-9]{3}\))?(([0-9]){2})?([0-9]){5}-([0-9]){4}"/>
-                    </div>
-    
-                    <div>
-                        <label for="">E-mail</label>
-                        <input type="email" name="email"  maxlength="30"/>
-                    </div>
-    
-                    <div>
-                        <label for="">Observação</label>
-                        <textarea name="obs" maxlength="100"></textarea>
-        
-                    </div>
-    
-                    
+        <form role="form" action="" method="post">
+            <div class="campos">
+                <div>
+                    <label for="">Nome</label>
+                    <input type="text" name="nome" maxlength="50" required/>
                 </div>
+
+                <div>
+                    <label for="">Telefone</label>
+                    <input type="tel" name="telefone" maxlength="17"
+                    pattern="(\([0-9]{3}\))?(([0-9]){2})?([0-9]){5}-([0-9]){4}"/>
+                </div>
+
+                <div>
+                    <label for="">E-mail</label>
+                    <input type="email" name="email"  maxlength="30"/>
+                </div>
+
+                <div>
+                    <label for="">Observação</label>
+                    <textarea name="obs" maxlength="100"></textarea>
+
+                </div>
+
                 
-                <button type="submit" class="botao_cadastrar">Salvar</button>
-            </form>
+            </div>
+            
+            <button type="submit" class="botao_cadastrar">Salvar</button>
+        </form>
 
-        </main>
-        
+    </main>
 
-        
+@endsection
 
-    </div>
-
-    
-</body>
-</html>
